9e871dc53440e777d495d6952ddad15222d0f8a6
tforteni <teniasade@gmail.com>
2024-09-03 16:36:35 -0400

    Added become a braider option

--- resources/views/braiders.blade.php
+++ resources/views/braiders.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     <div class="welcome-text mt-10 flex flex-col justify-center items-center">
         <p class="text-tahini text-4xl font-schoolbook">Meet our braiders!</p>
+        <a href="#" class="text-light text-xl font-schoolbook hover:underline">Or would you like to become a braider?</a>
         <div class="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach($braiders as $braider)
             <x-braider-card :braider="$braider" >
