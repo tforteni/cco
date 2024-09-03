7eb22ce99dc98d5c93b5f6fbb05b419d6f5e76fd
tforteni <teniasade@gmail.com>
2024-09-03 16:53:38 -0400

    Update braiders.blade.php

--- resources/views/braiders.blade.php
+++ resources/views/braiders.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="welcome-text mt-10 flex flex-col justify-center items-center">
         <p class="text-tahini text-4xl font-schoolbook">Meet our braiders!</p>
-        <a href="#" class="text-light text-xl font-schoolbook hover:underline">Or would you like to become a braider?</a>
+        <a href="#" class="text-light text-xl font-schoolbook hover:underline">Or would you like to become an affiliated braider?</a>
         <div class="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach($braiders as $braider)
             <x-braider-card :braider="$braider" >
@@ -10,4 +10,3 @@
         </div>
     </div>
 </x-layout>
-Tesrtin
