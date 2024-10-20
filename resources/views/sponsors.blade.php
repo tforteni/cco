6f8cc972a4ef39974de7a78645f521df855efcb3
tforteni <teniasade@gmail.com>
2024-10-19 20:01:46 -0400

    removed mielle

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -3,14 +3,14 @@
         <h1 class="text-4xl font-bold text-center text-tahini mb-8">Featured Sponsors</h1>
 
         <!-- Sponsor 1 -->
-        <div class="flex flex-col lg:flex-row items-start lg:items-center mb-12">
+        <!-- <div class="flex flex-col lg:flex-row items-start lg:items-center mb-12">
             <a href="https://mielleorganics.com" target="_blank" class="lg:mr-8">
                 <img src="{{ asset('images/mielle_logo.jpg') }}" alt="Mielle" class="h-40 w-40 object-contain">
             </a>
             <div class="lg:flex-grow lg:mt-0 lg:text-left">
                 <p class="text-sm text-tahini">Built on a mission to serve Black women with a high-performance product and natural ingredients.</p>
             </div>
-        </div>
+        </div> -->
 
         <!-- Sponsor 2 -->
         <div class="flex flex-col lg:flex-row items-start lg:items-center mb-12">
